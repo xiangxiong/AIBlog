ce7c939bad4c8ef8ddb71c95bc869bb14fd0c57e
shawn <shawn.xiang@kinochat.ai>
2026-03-20 18:07:12 +0800

    feat: 添加配置文件和更新示例代码

--- langchain/langchain_day01/ModelIO/helloworld.py
+++ langchain/langchain_day01/ModelIO/helloworld.py
@@ -1,27 +1,29 @@
-import os
 from openai import OpenAI
+from config import DEEPSEEK_API_KEY, DEEPSEEK_BASE_URL, DEEPSEEK_MODEL
 
 # 传统方式跟大模型交互
 # 创建大模型连接
-# client = OpenAI(
-#     # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
-#     api_key=os.getenv("DASHSCOPE_API_KEY"),
-#     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
-# )
-# completion = client.chat.completions.create(
-#     model="qwen-max",  # 此处以qwen-plus为例，可按需更换模型名称。模型列表：https://help.aliyun.com/zh/model-studio/getting-started/models
-#     messages=[
-#         {'role': 'system', 'content': 'You are a helpful assistant.'},
-#         {'role': 'user', 'content': '你是谁？'}],
-# )
-# # print(completion.model_dump_json())
-# print(completion.choices[0].message.content)
+client = OpenAI(
+    api_key=DEEPSEEK_API_KEY,
+    base_url=DEEPSEEK_BASE_URL,
+)
+completion = client.chat.completions.create(
+    model=DEEPSEEK_MODEL,
+    messages=[
+        {'role': 'system', 'content': 'You are a helpful assistant.'},
+        {'role': 'user', 'content': '你是谁？'}],
+)
+# print(completion.model_dump_json())
+print(completion.choices[0].message.content)
 
+# 使用 Langchain 方式交互
+from langchain_openai import ChatOpenAI
 
-# 使用Langchain方式交互
-from langchain_community.chat_models.tongyi import ChatTongyi
-
-model = ChatTongyi()
+model = ChatOpenAI(
+    openai_api_key=DEEPSEEK_API_KEY,
+    openai_api_base=DEEPSEEK_BASE_URL,
+    model_name=DEEPSEEK_MODEL
+)
 
 response = model.invoke("你是谁？")
 print(response.content)
